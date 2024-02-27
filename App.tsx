a6678c154b72ecc06581857e51f6200316497bf8
AnanthuMS <ananthums1721@gmail.com>
2024-02-27 11:10:01 +0530

    Bottom Tab Updated

--- App.tsx
+++ App.tsx
@@ -11,9 +11,14 @@ import {
 } from 'react-native';
 import {Provider} from 'react-redux';
 import reduxStore from './src/redux/store';
-import Routes from './src/Routes';
+import Routes from './src/Routes/HomeStack';
 import { NavigationContainer } from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import HomeStack from './src/Routes/HomeStack';
 import TabNavigator from './src/Routes/TabNavigator';
+const Stack = createStackNavigator();
+
+
 
 const height = Dimensions.get('window').height;
 const width = Dimensions.get('window').width;
@@ -21,8 +26,20 @@ const width = Dimensions.get('window').width;
 const App = () => {
   return (
     <Provider store={reduxStore}>
-      <Routes />
+      {/* <Routes /> */}
       {/* <TabNavigator /> */}
+      <NavigationContainer>
+          <Stack.Navigator initialRouteName="home">
+            <Stack.Screen
+              name="home"
+              component={TabNavigator}
+              options={{
+                headerShown: false,
+              }}
+            />
+          </Stack.Navigator>
+
+        </NavigationContainer>
 
     </Provider>
   );
--- src/Routes/TabNavigator.js
+++ src/Routes/TabNavigator.js
@@ -5,53 +5,79 @@ import Home from "../Screens/Home/Home";
 import Notifications from "../Screens/Notifications/Notifications";
 import { NavigationContainer } from "@react-navigation/native";
 import Colors from "../Theme/Colors";
-// import NotificationFSVG from "../assets/svg/NotificationFSVG";
+import NotificationFSVG from "../assets/svg/NotificationFSVG";
 import images from "../assets/Images";
+import Reels from "../Screens/Reels/Reels";
+import Contests from "../Screens/Contests/Contests";
 
 const Tab = createBottomTabNavigator();
 
 function TabNavigator({ title }) {
   return (
-    <NavigationContainer>
-      <Tab.Navigator
-        initialRouteName="Home"
-        screenOptions={{
-          headerTitleAlign: "center",
-          //   tabBarActiveTintColor: Colors.primary,
-          // tabBarInactiveTintColor: Colors.WHITE,
-          // headerTitleStyle: { ...Typography.largeTextBold },
-          // tabBarActiveBackgroundColor: Colors.BLACK,
-          // tabBarInactiveBackgroundColor: Colors.BLACK,
+    <Tab.Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        headerTitleAlign: "center",
+        tabBarActiveTintColor: Colors.primary,
+        // tabBarInactiveTintColor: Colors.WHITE,
+        // headerTitleStyle: { ...Typography.largeTextBold },
+        tabBarActiveBackgroundColor: Colors.white,
+        tabBarInactiveBackgroundColor: Colors.white,
+      }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          title: "Home",
+          headerShown: false,
+          tabBarIcon: ({ color }) => {
+            return <NotificationFSVG height={20} width={20} color={color} />;
+          },
+          unmountOnBlur: true,
+          tabBarHideOnKeyboard: true,
         }}
-      >
-        <Tab.Screen
-          name="Home"
-          component={Home}
-          options={{
-            title: "Home",
-            headerShown: false,
-            // tabBarIcon: ({ color }) => {
-            //   return <NotificationFSVG height={20} width={20} color={color} />;
-            // },
-            unmountOnBlur: true,
-            tabBarHideOnKeyboard: true,
-          }}
-        />
-        <Tab.Screen
-          name="Notifications"
-          component={Notifications}
-          options={{
-            title: "Notifications",
-            headerShown: false,
-            // tabBarIcon: ({ color }) => {
-            //   //   return <NotificationFSVG height={20} width={20} color={color} />;
-            // },
-            unmountOnBlur: true,
-            tabBarHideOnKeyboard: true,
-          }}
-        />
-      </Tab.Navigator>
-    </NavigationContainer>
+      />
+      <Tab.Screen
+        name="Reels"
+        component={Reels}
+        options={{
+          title: "Reels",
+          headerShown: false,
+          tabBarIcon: ({ color }) => {
+            return <NotificationFSVG height={20} width={20} color={color} />;
+          },
+          unmountOnBlur: true,
+          tabBarHideOnKeyboard: true,
+        }}
+      />
+      <Tab.Screen
+        name="Contests"
+        component={Contests}
+        options={{
+          title: "Contests",
+          headerShown: false,
+          tabBarIcon: ({ color }) => {
+            return <NotificationFSVG height={20} width={20} color={color} />;
+          },
+          unmountOnBlur: true,
+          tabBarHideOnKeyboard: true,
+        }}
+      />
+      <Tab.Screen
+        name="Notifications"
+        component={Notifications}
+        options={{
+          title: "Notifications",
+          headerShown: false,
+          tabBarIcon: ({ color }) => {
+            return <NotificationFSVG height={20} width={20} color={color} />;
+          },
+          unmountOnBlur: true,
+          tabBarHideOnKeyboard: true,
+        }}
+      />
+    </Tab.Navigator>
   );
 }
 
