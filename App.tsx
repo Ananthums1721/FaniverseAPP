c6bbcd5a9b510094ec8c4f702375a988eb215546
AnanthuMS <ananthums1721@gmail.com>
2024-03-11 15:02:49 +0530

    New Commit

--- App.tsx
+++ App.tsx
@@ -1,14 +1,5 @@
-import React, {useState} from 'react';
-import {
-  View,
-  StyleSheet,
-  Text,
-  SafeAreaView,
-  Dimensions,
-  Image,
-  TouchableOpacity,
-  ScrollView,
-} from 'react-native';
+import React from 'react';
+import { Dimensions} from 'react-native';
 import {Provider} from 'react-redux';
 import reduxStore from './src/redux/store';
 import Routes from './src/Routes/HomeStack';
